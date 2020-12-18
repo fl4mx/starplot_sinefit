409143ff54e7fa7f02cb12b238530f9648953ea1
Michael Chen <45020248+fl4mx@users.noreply.github.com>
2020-12-18 21:52:21 +1100

    initial commit
    adding main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import math
+import statistics
+import numpy as np
+import matplotlib.pyplot as plt
+from lmfit import Parameters, fit_report, minimize
+
+#data locations
+LCdir = r"C:\Users\micha\Documents\OGLE-ATLAS-RR-c\sample_DATs"
+stardata = r"C:\Users\micha\Documents\OGLE-ATLAS-RR-c\star_data.txt"
+
+#reading all star data from DAT file (names and periods)
+names = np.loadtxt(stardata, dtype=str, skiprows=7, usecols=0)
+periods = np.loadtxt(stardata, skiprows=7, usecols=8)
+
+LCfilelist = os.listdir(LCdir)
+#iterate through star LC
+for countLC, file in enumerate(LCfilelist):
+    #reading LC data from LC files (dates and brightness)
+    dates = np.loadtxt(LCdir + "\\" + file, delimiter=" ", usecols=0)
+    brightnesses = np.loadtxt(LCdir + "\\" + file, delimiter=" ", usecols=1)
+    #grabbing relevant star data for current star (name, starting time, period) from DAT
+    name = names[countLC]
+    starting_date = dates[0]
+    period = periods[countLC]
+
+    #simple phasing calculation
+    phases = ((dates - starting_date) / period) % 1
+    #mean value taken as the arithmetic mean of all y-values
+    average = statistics.mean(brightnesses)
+    #better amplitude is the mean of the differences between average and max/min
+    amplitude = statistics.mean([(max(brightnesses) - average), average - min(brightnesses)])
+    #check for the closest value to the mean value of brightness (mid_brightness), find its corresponding x value (mid_index, mid_phase)
+    brightness_diff = lambda checkbrightness: abs(checkbrightness - average)
+    mid_brightness = min(brightnesses, key=brightness_diff)
+    mid_index = np.where(brightnesses == mid_brightness)[0][0]
+    mid_phase = phases[mid_index]
+
+    #trig transform onto the plot
+    sine_brightnesses = (amplitude * np.sin(((phases - mid_phase)* 2 * math.pi))) + average
+
+    #calculate root mean square deviation from sine curve of LC
+    rms_diff = math.sqrt((1 / len(sine_brightnesses)) * sum(abs(sine_brightnesses - brightnesses) ** 2))
+    print(str(rms_diff))
+
+    #plot the LC, sine curve
+    plt.scatter(phases, brightnesses)
+    plt.xlabel("Phase")
+    plt.ylabel("Magnitude")
+    plt.title(name)
+    plt.scatter(phases, sine_brightnesses)
+    plt.show()
